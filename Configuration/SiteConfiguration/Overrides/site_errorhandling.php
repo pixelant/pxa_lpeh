8dd73c044ee0eeab4b8c4a56e3fad277ed7ed3db
mabolek <mathias@lilio.com>
2020-06-10 09:06:11 +0200

    [FEATURE] Override default Page Content error handler by default
    Can be disabled through extension configuration.

--- Configuration/SiteConfiguration/Overrides/site_errorhandling.php
+++ Configuration/SiteConfiguration/Overrides/site_errorhandling.php
@@ -1,11 +1,19 @@
 <?php
-// Show both errorPhpClassFQCN and errorContentSource for PHP Class error handler
-$currentShowItem = $GLOBALS['SiteConfiguration']['site_errorhandling']['types']['PHP']['showitem'];
-if (strpos($currentShowItem, 'errorContentSource') === false) {
-    // Add errorContentSource for PHP Class error handler if not already present
-    $GLOBALS['SiteConfiguration']['site_errorhandling']['types']['PHP']['showitem'] = str_replace(
-        'errorPhpClassFQCN',
-        'errorPhpClassFQCN, errorContentSource',
-        $GLOBALS['SiteConfiguration']['site_errorhandling']['types']['PHP']['showitem']
-    );
+
+if (
+    !TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+    )
+    ->get('pxa_lpeh', 'disableXClass')
+) {
+    // Show both errorPhpClassFQCN and errorContentSource for PHP Class error handler
+    $currentShowItem = $GLOBALS['SiteConfiguration']['site_errorhandling']['types']['PHP']['showitem'];
+    if (strpos($currentShowItem, 'errorContentSource') === false) {
+        // Add errorContentSource for PHP Class error handler if not already present
+        $GLOBALS['SiteConfiguration']['site_errorhandling']['types']['PHP']['showitem'] = str_replace(
+            'errorPhpClassFQCN',
+            'errorPhpClassFQCN, errorContentSource',
+            $GLOBALS['SiteConfiguration']['site_errorhandling']['types']['PHP']['showitem']
+        );
+    }
 }
